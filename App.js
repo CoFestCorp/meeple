e40b40be12489a2860dfbafc66ca1a3d2917fbab
gulla01 <65980604+gulla01@users.noreply.github.com>
2020-07-05 19:34:37 +0100

    joe

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import {
   StyleSheet,
   Image,
@@ -10,47 +10,46 @@ import {
   View,
   Text,
   TouchableOpacity,
-  AppRegistry
-} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import stylesApp from './css';
-import stylesLogIn from './css';
-import styles from './css';
-
-
-
+  AppRegistry,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import stylesApp from "./css";
+import stylesLogIn from "./css";
+import styles from "./css";
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.1 };
 
-
-
-
-
-
 function HomeScreen({ navigation }) {
   return (
-
     <SafeAreaView style={styles.container}>
-      <Image style={styles.logo}
+      <Image
+        style={styles.logo}
         fadeDuration={1000}
-        source={require('./Images/logo_transparent.png')}
+        source={require("./Images/logo_transparent.png")}
       />
 
-      <TouchableOpacity onPress={() => navigation.navigate('Register')} style={styles.login}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Register")}
+        style={styles.login}
+      >
         <Text style={styles.loginG}>Get Started</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.login2}>
-
-
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Login")}
+        style={styles.login2}
+      >
         <Text style={styles.loginT}>Log in</Text>
       </TouchableOpacity>
       <View style={styles.btm}>
-        <Text style={{ fontSize: 20, }}>New around here ?</Text>
-
+        <Text style={{ fontSize: 20 }}>Forgotten password ?</Text>
       </View>
+
       <View style={styles.btm2}>
-        <Button onPress={() => navigation.navigate('QRegister')} title="Sign up"></Button>
+        <Button
+          onPress={() => navigation.navigate("QRegister")}
+          title="Recovery"
+        ></Button>
       </View>
     </SafeAreaView>
   );
@@ -65,17 +64,19 @@ function LoginScreen() {
       <View style={styles.login2}>
         <TextInput style={styles.loginP}></TextInput>
       </View>
-      <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.login2}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Login")}
+        style={styles.login2}
+      >
         <Text style={styles.loginT}>Log in</Text>
       </TouchableOpacity>
-
     </SafeAreaView>
   );
 }
 
 function RegisterScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Register Screen</Text>
     </View>
   );
@@ -83,8 +84,31 @@ function RegisterScreen() {
 
 function QRegisterScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>QRegister Screen</Text>
+    <View style={styles.QRegScreen}>
+      <Image
+        style={styles.Recoverylogo}
+        fadeDuration={1000}
+        source={require("./Images/logo_transparent.png")}
+      />
+
+      <Text style={styles.RecoveryText}>Password Recovery</Text>
+      <Text style={styles.RecoveryInfo}>
+        If you have lost your account password, simply enter your email address
+        and phone number below. Directions for reseting your password will be
+        emailed to the address provided.
+      </Text>
+      <Text style={styles.EmailTitle}>Email address</Text>
+      <TextInput style={styles.EmailInput}></TextInput>
+
+      <Text style={styles.PhoneNumber}>Phone number</Text>
+      <TextInput style={styles.PhoneInput}></TextInput>
+
+      <TouchableOpacity
+        onPress={() => alert("This does absoultly nothing !")}
+        style={styles.RecoverySubmit}
+      >
+        <Text style={styles.submitText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -95,21 +119,36 @@ class Hidden extends React.Component {
   }
 }
 
-
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} styles={stylesApp.title} name="Home" component={HomeScreen} />
-        <Stack.Screen styles={stylesApp.title} name="Login" component={LoginScreen} />
-        <Stack.Screen styles={stylesApp.title} name="QRegister" component={QRegisterScreen} />
-        <Stack.Screen styles={stylesApp.title} name="Register" component={RegisterScreen} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          styles={stylesApp.title}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          styles={stylesApp.title}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          styles={stylesApp.title}
+          name="QRegister"
+          component={QRegisterScreen}
+        />
+        <Stack.Screen
+          styles={stylesApp.title}
+          name="Register"
+          component={RegisterScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 
 export default App;
--- css.js
+++ css.js
@@ -1,132 +1,205 @@
-import 'react-native-gesture-handler';
-import {
-    StyleSheet,
-} from 'react-native';
-
-
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
 
 const stylesApp = StyleSheet.create({
-    title: {
-        marginTop: 50,
-    }
+  title: {
+    marginTop: 50,
+  },
 });
-
-
 
 const stylesLogIn = StyleSheet.create({
-    container: {
-        width: 420,
-        height: 1000,
-        backgroundColor: '#FCD2FF',
-    },
+  container: {
+    width: 420,
+    height: 1000,
+    backgroundColor: "#FCD2FF",
+  },
 
-    title: {
-        left: 50,
-    },
+  title: {
+    left: 50,
+  },
 
-    login2: {
-        width: 305,
-        height: 58,
-        left: 55,
-        top: 520,
-        backgroundColor: 'white',
-        borderRadius: 24,
-        shadowOffset: { width: 5, height: 5, },
-        shadowColor: 'black',
-        shadowOpacity: 0.1,
-    },
+  login2: {
+    width: 305,
+    height: 58,
+    left: 55,
+    top: 520,
+    backgroundColor: "white",
+    borderRadius: 24,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+  },
 
-    loginT: {
-        color: 'black',
-        fontSize: 20,
-        lineHeight: 25,
-        left: 125,
-        bottom: -15,
+  loginT: {
+    color: "black",
+    fontSize: 20,
+    lineHeight: 25,
+    left: 125,
+    bottom: -15,
+  },
 
-    },
-
-    loginU: {
-
-    },
-    loginP: {
-
-    }
-
+  loginU: {},
+  loginP: {},
 });
 
-
-
 const styles = StyleSheet.create({
+  container: {
+    width: 420,
+    height: 1000,
+    backgroundColor: "#FCD2FF",
+  },
 
+  logo: {
+    width: 375,
+    height: 472,
+    left: 17,
+    top: 0,
+  },
 
-    container: {
-        width: 420,
-        height: 1000,
-        backgroundColor: '#FCD2FF',
-    },
+  login: {
+    width: 305,
+    height: 58,
+    left: 55,
+    top: -40,
+    backgroundColor: "#9b4ae6",
+    borderRadius: 24,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+  },
 
-    logo: {
-        width: 375,
-        height: 472,
-        left: 17,
-        top: -50
-    },
+  loginG: {
+    color: "#FFFFFF",
+    fontSize: 25,
+    fontWeight: "bold",
+    lineHeight: 25,
+    left: 90,
+    bottom: -20,
+  },
 
-    login: {
-        width: 305,
-        height: 58,
-        left: 55,
+  login2: {
+    width: 305,
+    height: 58,
+    left: 55,
+    top: -20,
+    backgroundColor: "white",
+    borderRadius: 24,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+  },
 
-        backgroundColor: '#9b4ae6',
-        borderRadius: 24,
-        shadowOffset: { width: 5, height: 5, },
-        shadowColor: 'black',
-        shadowOpacity: 0.1,
-    },
+  loginT: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 25,
+    lineHeight: 25,
+    left: 117,
+    bottom: -20,
+  },
 
-    loginG: {
-        color: '#FFFFFF',
-        fontSize: 20,
-        lineHeight: 25,
-        left: 105,
-        bottom: -15,
+  btm: {
+    top: 40,
+    left: 110,
+    width: 195,
+  },
 
-    },
+  btm2: {
+    top: 45,
+    left: 126,
+    width: 165,
+  },
 
-    login2: {
-        width: 305,
-        height: 58,
-        left: 55,
-        top: 10,
-        backgroundColor: 'white',
-        borderRadius: 24,
-        shadowOffset: { width: 5, height: 5, },
-        shadowColor: 'black',
-        shadowOpacity: 0.1,
-    },
-    loginT: {
-        color: 'black',
-        fontSize: 20,
-        lineHeight: 25,
-        left: 125,
-        bottom: -15,
+  QRegScreen: {
+    backgroundColor: "#FCD2FF",
+    flex: 1,
+    alignItems: "center",
+  },
 
-    },
+  RecoveryText: {
+    color: "white",
+    backgroundColor: "#9b4ae6",
+    fontSize: 30,
+    fontWeight: "bold",
+    width: 400,
+    height: 45,
+    top: -77,
+    textAlign: "center",
+  },
 
-    btm: {
-        top: 40,
-        left: 125,
+  PhoneNumber: {
+    color: "black",
+    fontSize: 25,
+    width: 165,
+    height: 30,
+    top: 0,
+    left: -95,
+  },
 
-    },
+  EmailTitle: {
+    color: "black",
+    fontSize: 25,
+    width: 160,
+    height: 30,
+    top: -25,
+    left: -100,
+  },
 
-    btm2: {
-        top: 40,
-        left: -5,
-    }
+  EmailInput: {
+    backgroundColor: "white",
+    width: 250,
+    height: 40,
+    borderColor: "black",
+    borderWidth: 2,
+    top: -20,
+    left: -55,
+  },
 
+  PhoneInput: {
+    backgroundColor: "white",
+    width: 250,
+    height: 40,
+    borderColor: "black",
+    borderWidth: 2,
+    top: 5,
+    left: -55,
+  },
 
+  RecoverySubmit: {
+    width: 260,
+    height: 58,
+    backgroundColor: "white",
+    top: 180,
+    left: 0,
+    borderRadius: 24,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+  },
 
+  submitText: {
+    textAlign: "center",
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 25,
+    lineHeight: 25,
+    bottom: -20,
+  },
 
+  Recoverylogo: {
+    width: 175,
+    height: 172,
+    left: 0,
+    top: -25,
+  },
+
+  RecoveryInfo: {
+    fontWeight: "bold",
+    fontSize: 15,
+    width: 380,
+    right: 0,
+    top: -50,
+  },
 });
 
 export default styles;
