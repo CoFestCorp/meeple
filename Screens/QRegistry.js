77463fe1ff77412a189d8a583bbc6e0f036966bf
gulla01 <65980604+gulla01@users.noreply.github.com>
2020-07-07 12:41:32 +0100

    Recovery screens

--- Screens/QRegistry.js
+++ Screens/QRegistry.js
@@ -1,46 +1,46 @@
 import "react-native-gesture-handler";
 import * as React from "react";
-import {
-    Image,
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-} from "react-native";
+import { Image, View, Text, TextInput, TouchableOpacity } from "react-native";
 import styles from "../css";
 import App from "../App";
 
+//changes made this this function
+function QRegisterScreen({ navigation }) {
+  return (
+    <View style={styles.QRegScreen}>
+      <Image
+        style={styles.Recoverylogo}
+        fadeDuration={1000}
+        source={require("../Images/logo_transparent.png")}
+      />
 
-
-function QRegisterScreen() {
-    return (
-        <View style={styles.QRegScreen}>
-            <Image
-                style={styles.Recoverylogo}
-                fadeDuration={1000}
-                source={require("../Images/logo_transparent.png")}
-            />
-
-            <Text style={styles.RecoveryText}>Password Recovery</Text>
-            <Text style={styles.RecoveryInfo}>
-                If you have lost your account password, simply enter your email address
-                and phone number below. Directions for reseting your password will be
-                emailed to the address provided.
+      <Text style={styles.RecoveryText}>Forgot your password ?</Text>
+      <Text style={styles.RecoveryInfo}>
+        Don't worry type your email here and we'll recover it for you.
       </Text>
-            <Text style={styles.EmailTitle}>Email address</Text>
-            <TextInput style={styles.EmailInput}></TextInput>
 
-            <Text style={styles.PhoneNumber}>Phone number</Text>
-            <TextInput style={styles.PhoneInput}></TextInput>
+      <Text style={styles.EmailTitle}>Email address</Text>
+      <TextInput
+        placeholder="Email address"
+        style={styles.EmailInput}
+      ></TextInput>
 
-            <TouchableOpacity
-                onPress={() => alert("This does absoultly nothing !")}
-                style={styles.RecoverySubmit}
-            >
-                <Text style={styles.submitText}>Submit</Text>
-            </TouchableOpacity>
-        </View>
-    );
+      <TouchableOpacity
+        onPress={() => navigation.navigate("PhoneNumberRecovery")}
+        style={styles.ForgotEmailbtnDesign}
+      >
+        <Text style={styles.ForgotEmailbtn}>Forgot your Email ?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("RecoveryCode")}
+        style={styles.RecoverySubmit}
+      >
+        <Text style={styles.submitText}>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+//nothing below here
 
 export default QRegisterScreen;
