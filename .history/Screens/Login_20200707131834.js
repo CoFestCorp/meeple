b7f1a6529b96af88e208653a17c11753c12fd105
PhilippRomanovskiy <67516815+PhilippRomanovskiy@users.noreply.github.com>
2020-07-07 13:43:30 +0200

    changes

---
+++ .history/Screens/Login_20200707131834.js
@@ -0,0 +1,45 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import {
+  Image,
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import styles2 from "../css";
+import App from "../App";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+
+function LoginScreen({ navigation }) {
+  return (
+    <SafeAreaView style={stylesLogIn.container}>
+      <Image
+        style={styles.loginLogo}
+        fadeDuration={1000}
+        source={require("../Images/logo_transparent.png")}
+      />
+      <View style={styles.details}>
+        <TextInput placeholder="username or email" style={styles.input} />
+        <TextInput
+          placeholder="password"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.icon}>
+          <Icon name="eye" size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("UserHome")}
+        style={styles.login3}
+      >
+        <Text style={styles.loginT2}>Log in</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+export default LoginScreen;
---
+++ .history/Screens/Login_20200707133644.js
@@ -0,0 +1,52 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+
+import {
+  Image,
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Item,
+  Input,
+  Logo
+} from "react-native";
+import styles from "../css";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+
+
+function LoginScreen({ navigation }) {
+  state = {
+    password:' ',
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.loginLogo}
+        fadeDuration={1000}
+        source={require("../Images/logo_transparent.png")}
+      />
+      <View style={styles.details}>
+        <TextInput placeholder="username or email" style={styles.input} />
+        <PasswordInputText
+         // placeholder="password"
+          //style={styles.input}
+          //secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.icon}>
+          <Icon name="eye" size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("UserHome")}
+        style={styles.login3}
+      >
+        <Text style={styles.loginT2}>Log in</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+export default LoginScreen;
