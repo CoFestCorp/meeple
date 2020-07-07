07f5cca66d009c466833dc2e6e5feb9f44a2ba53
andreadhelpra <62818869+andreadhelpra@users.noreply.github.com>
2020-07-07 13:52:28 +0200

    andrea

--- Screens/UserHome.js
+++ Screens/UserHome.js
@@ -1,13 +1,11 @@
 import "react-native-gesture-handler";
 import * as React from "react";
 import { View, Text, Button, TouchableOpacity, Image } from "react-native";
-import SwipeCards from "./SwipeCards.js";
 
 function UserHomeScreen({ navigation }) {
   return (
     <View>
       <Text>Hello</Text>
-      <SwipeCards style={{ flex: 1 }} />
     </View>
   );
 }
