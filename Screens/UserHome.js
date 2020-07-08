55dc7e9d9113b63f1bd26261288a2ae978ad783b
andreadhelpra <62818869+andreadhelpra@users.noreply.github.com>
2020-07-08 21:51:56 +0200

    Swipe cards

--- Screens/UserHome.js
+++ Screens/UserHome.js
@@ -2,9 +2,14 @@ import "react-native-gesture-handler";
 import * as React from "react";
 import { View, Text, Button, TouchableOpacity, Image } from "react-native";
 import styles from "../css";
+import SwipeCard from "../Components/SwipeCards.js";
 
 function UserHomeScreen({ navigation }) {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <SwipeCard />
+    </View>
+  );
 }
 
 export default UserHomeScreen;
